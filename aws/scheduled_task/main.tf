897cce15f84c7e6bc298cb152d2433106f4b142c
Halfdan Rump <dalekk@gmail.com>
2019-08-01 17:57:14 +0900

    Uses default IAM roles for task execution and scheduling

--- aws/scheduled_task/main.tf
+++ aws/scheduled_task/main.tf
@@ -1,47 +1,47 @@
 ## ECS task exectution role
 
-resource "aws_iam_role" "scheduled_task_ecs_execution" {
-  name               = "${var.name}-${var.environment}-st-ecs-execution-role"
-  assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-execution-assume-role-policy.json")}"
-}
+# resource "aws_iam_role" "scheduled_task_ecs_execution" {
+#   name               = "${var.name}-${var.environment}-st-ecs-execution-role"
+#   assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-execution-assume-role-policy.json")}"
+# }
 
-data "template_file" "scheduled_task_ecs_execution_policy" {
-  template = "${file("${path.module}/policies/scheduled-task-ecs-execution-policy.json")}"
-}
-
-resource "aws_iam_role_policy" "scheduled_task_ecs_execution" {
-  name   = "${var.name}-${var.environment}-st-ecs-execution-policy"
-  role   = "${aws_iam_role.scheduled_task_ecs_execution.id}"
-  policy = "${data.template_file.scheduled_task_ecs_execution_policy.rendered}"
-}
+# data "template_file" "scheduled_task_ecs_execution_policy" {
+#   template = "${file("${path.module}/policies/scheduled-task-ecs-execution-policy.json")}"
+# }
+#
+# resource "aws_iam_role_policy" "scheduled_task_ecs_execution" {
+#   name   = "${var.name}-${var.environment}-st-ecs-execution-policy"
+#   role   = "${aws_iam_role.scheduled_task_ecs_execution.id}"
+#   policy = "${data.template_file.scheduled_task_ecs_execution_policy.rendered}"
+# }
 
 ## ECS task role
 
-resource "aws_iam_role" "scheduled_task_ecs" {
-  name               = "${var.name}-${var.environment}-st-ecs-role"
-  assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-assume-role-policy.json")}"
-}
+# resource "aws_iam_role" "scheduled_task_ecs" {
+#   name               = "${var.name}-${var.environment}-st-ecs-role"
+#   assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-assume-role-policy.json")}"
+# }
 
 ## Cloudwatch event role
 
-resource "aws_iam_role" "scheduled_task_cloudwatch" {
-  name               = "${var.name}-${var.environment}-st-cloudwatch-role"
-  assume_role_policy = "${file("${path.module}/policies/scheduled-task-cloudwatch-assume-role-policy.json")}"
-}
+# resource "aws_iam_role" "scheduled_task_cloudwatch" {
+#   name               = "${var.name}-${var.environment}-st-cloudwatch-role"
+#   assume_role_policy = "${file("${path.module}/policies/scheduled-task-cloudwatch-assume-role-policy.json")}"
+# }
 
-data "template_file" "scheduled_task_cloudwatch_policy" {
-  template = "${file("${path.module}/policies/scheduled-task-cloudwatch-policy.json")}"
-
-  vars {
-    task_execution_role_arn = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
-  }
-}
-
-resource "aws_iam_role_policy" "scheduled_task_cloudwatch_policy" {
-  name   = "${var.name}-${var.environment}-st-cloudwatch-policy"
-  role   = "${aws_iam_role.scheduled_task_cloudwatch.id}"
-  policy = "${data.template_file.scheduled_task_cloudwatch_policy.rendered}"
-}
+# data "template_file" "scheduled_task_cloudwatch_policy" {
+#   template = "${file("${path.module}/policies/scheduled-task-cloudwatch-policy.json")}"
+#
+#   vars {
+#     task_execution_role_arn = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
+#   }
+# }
+#
+# resource "aws_iam_role_policy" "scheduled_task_cloudwatch_policy" {
+#   name   = "${var.name}-${var.environment}-st-cloudwatch-policy"
+#   role   = "${aws_iam_role.scheduled_task_cloudwatch.id}"
+#   policy = "${data.template_file.scheduled_task_cloudwatch_policy.rendered}"
+# }
 
 ## CloudWatch log group
 resource "aws_cloudwatch_log_group" "task_logs" {
@@ -52,16 +52,14 @@ resource "aws_cloudwatch_log_group" "task_logs" {
   }
 }
 
-
-## ECS task definition
-
 resource "aws_ecs_task_definition" "scheduled_task" {
   family                   = "${var.name}_${var.environment}_scheduled_task"
   container_definitions    = "${var.container_definitions}"
   requires_compatibilities = ["${var.launch_type}"]
   network_mode             = "${var.network_mode}"
-  execution_role_arn       = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
-  task_role_arn            = "${aws_iam_role.scheduled_task_ecs.arn}"
+  # execution_role_arn       = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
+  execution_role_arn = "arn:aws:iam::${var.account_id}:role/ecsTaskExecutionRole"
+  # task_role_arn            = "${aws_iam_role.scheduled_task_ecs.arn}"
   cpu                      = "${var.cpu}"
   memory                   = "${var.memory}"
 }
@@ -78,7 +76,8 @@ resource "aws_cloudwatch_event_target" "scheduled_task" {
   target_id = "${var.name}_${var.environment}_scheduled_task_target"
   rule      = "${aws_cloudwatch_event_rule.scheduled_task.name}"
   arn       = "${var.cluster_arn}"
-  role_arn  = "${aws_iam_role.scheduled_task_cloudwatch.arn}"
+  # role_arn  = "${aws_iam_role.scheduled_task_cloudwatch.arn}"
+  role_arn  = "arn:aws:iam::${var.account_id}:role/ecsEventsRole"
 
   ecs_target {
     task_count          = "${var.task_count}"
